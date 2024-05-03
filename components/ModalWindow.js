922833671e2e65b3be53c354089eb036e03dd15c
Nazarov Ruslan <rnazarov142@gmail.com>
2024-05-03 13:13:06 +0200

    update modal window and text

--- components/ModalWindow.js
+++ components/ModalWindow.js
@@ -26,12 +26,12 @@ export default function ModalWindow() {
                 <div className='absolute top-0 left-0 right-0 bottom-0 flex items-center justify-center z-50'>
                     <div className='relative p-5 shadow-2xl z-50 flex flex-col items-center border xs:w-[370px] rounded-lg border-[#6e2fba] bg-black'>
                         <Image 
-                            src={'/rasplogo.jpg'}
+                            src={'/rasplogo-vesn.jpg'}
                             width={100}
                             height={100}
                             alt="Логотип расписания"
                         />
-                        <p className='text-center font-mono mb-[70px]'>Привет, это сайт Расписание занятий для КБК, мы очень долго писали этот проект и этот день настал, мы решили что этот проект будет анонимный и никто не узнает кто его написал, один человек этим всем владеет :) Теперь ты точно будешь знать какие у тебя пары! <br /> Версия: 2.5 (beta) - все подробности в нашей официальной группе телеграмма :)</p>
+                        <p className='text-center font-mono mb-[70px]'>Привет, это сайт Расписание занятий для КБК, мы очень долго писали этот проект, и Мы решили, что этот проект будет анонимный и никто не узнает кто его написал, один человек этим всем владеет :) Теперь ты точно будешь знать какие у тебя пары! <br /> Версия: 2.5 (beta) - все подробности в нашей официальной группе телеграмма :)</p>
                         <button onClick={handleClose} className='mb-[20px] border border-[#6e2fba] px-[100px] py-[20px] rounded-lg bg-[#6e2fba] hover:bg-none'>Продолжить</button>
                     </div>
                     <div className='absolute inset-0 bg-black opacity-50 blur backdrop-blur-lg'></div>
