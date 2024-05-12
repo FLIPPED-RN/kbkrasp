76de0c11ab8e705b258f1bc1e1a2e0caab4671d9
Ruslan Nazarov <rnazarov142@gmail.com>
2024-05-12 12:28:20 +0200

    update 2.5 (beta) new text

--- components/HappyOpen.js
+++ components/HappyOpen.js
@@ -4,6 +4,8 @@ export default function HappyOpen() {
     return(
         <div className="flex flex-col xs:w-[370px] mx-auto sm:w-[550px] md:w-[750px] lg:w-[950px] xl:w-[1150px] text-center">
             {/* <h1 className="font-mono mx-auto text-2xl xs:text-[16px] sm:text-[18px] md:text-[20px] lg:text-[23px]">Все новости сайта здесь</h1> */}
+            <p className="mb-6 font-mono">В нашем канале мы написали - почему стоит выбрать наше расписание</p>
+            <p className="mb-6 font-mono text-[#6e2fba]">скоро новая фаза карточек</p>
             <a href="https://t.me/kbk_rasp" className="font-mono mx-auto text-center mb-10 px-10 py-3 text-[18px] border border-[#6e2fba] rounded-lg transition-all hover:border-[#fff] hover:text-[#6e2fba] xs:text-[16px] sm:text-[18px] md:text-[20px] lg:text-[23px] flex items-center gap-[20px]">
                 Наш официальный канаЛ
             </a> 
