22b31f8b63a847c19ba80c0d13bf59bbae7daad8
Ruslan Nazarov <rnazarov142@gmail.com>
2024-05-12 12:06:18 +0200

    update 2.5 (beta) new rasp

--- app/layout.js
+++ app/layout.js
@@ -16,7 +16,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <TehnWork />
+        {/* <TehnWork /> */}
         <header>
           <Header />
         </header>
--- components/HappyOpen.js
+++ components/HappyOpen.js
@@ -2,15 +2,15 @@ import Image from "next/image"
 
 export default function HappyOpen() {
     return(
-        // <div className="flex flex-col xs:w-[370px] mx-auto sm:w-[550px] md:w-[750px] lg:w-[950px] xl:w-[1150px] text-center">
-        //     {/* <h1 className="font-mono mx-auto text-2xl xs:text-[16px] sm:text-[18px] md:text-[20px] lg:text-[23px]">Все новости сайта здесь</h1> */}
-        //     <a href="https://t.me/kbk_rasp" className="font-mono mx-auto text-center mb-10 px-10 py-3 text-[18px] border border-[#6e2fba] rounded-lg transition-all hover:border-[#fff] hover:text-[#6e2fba] xs:text-[16px] sm:text-[18px] md:text-[20px] lg:text-[23px] flex items-center gap-[20px]">
-        //         Наш официальный канаЛ
-        //     </a> 
-        // </div>
         <div className="flex flex-col xs:w-[370px] mx-auto sm:w-[550px] md:w-[750px] lg:w-[950px] xl:w-[1150px] text-center">
-            <p className="font-mono mb-6 ">Вы можете поддержать наш проект</p>
-            <iframe src="https://yoomoney.ru/quickpay/fundraise/button?billNumber=12J2PH3MCHH.240507&" width="330" height="50" frameborder="0" allowtransparency="true" scrolling="no" className="mx-auto mb-6"></iframe>​
+            {/* <h1 className="font-mono mx-auto text-2xl xs:text-[16px] sm:text-[18px] md:text-[20px] lg:text-[23px]">Все новости сайта здесь</h1> */}
+            <a href="https://t.me/kbk_rasp" className="font-mono mx-auto text-center mb-10 px-10 py-3 text-[18px] border border-[#6e2fba] rounded-lg transition-all hover:border-[#fff] hover:text-[#6e2fba] xs:text-[16px] sm:text-[18px] md:text-[20px] lg:text-[23px] flex items-center gap-[20px]">
+                Наш официальный канаЛ
+            </a> 
         </div>
+        // <div className="flex flex-col xs:w-[370px] mx-auto sm:w-[550px] md:w-[750px] lg:w-[950px] xl:w-[1150px] text-center">
+        //     <p className="font-mono mb-6 ">Вы можете поддержать наш проект</p>
+        //     <iframe src="https://yoomoney.ru/quickpay/fundraise/button?billNumber=12J2PH3MCHH.240507&" width="330" height="50" frameborder="0" allowtransparency="true" scrolling="no" className="mx-auto mb-6"></iframe>​
+        // </div>
     )
 }
